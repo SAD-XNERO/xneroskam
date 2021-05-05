c4cc92480540920ea7a3d87846d5210224fe224c
SAD-XNERO <rayanalnatoure@gmail.com>
2021-05-05 08:09:01 -0400

    sad12

---
+++ Logo.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#clolors
+white='\e[1;37m'
+green='\e[0;32m'
+blue='\e[1;34m'
+red='\e[1;31m'
+yellow='\e[1;33m' 
+echo ""
+echo""
+banner() {
+echo -e $'\e[1;33m\e[0m\e[1;37m   █▀▀ █▀▀█ █▀▀▄   \e[0m''\e[1;33m\e[0m\e[1;32m  █░█ █▀▀▄ █▀▀ █▀▀█ █▀▀█ \e[0m'
+ echo -e $'\e[1;33m\e[0m\e[1;37m  ▀▀█ █▄▄█ █░░█    \e[0m''\e[1;33m\e[0m\e[1;32m ▄▀▄ █░░█ █▀▀ █▄▄▀ █░░█ \e[0m'
+ echo -e $'\e[1;33m\e[0m\e[1;37m  ▀▀▀ ▀░░▀ ▀▀▀░    \e[0m''\e[1;33m\e[0m\e[1;32m ▀░▀ ▀░░▀ ▀▀▀ ▀░▀▀ ▀▀▀▀ \e[0m'
+  echo""    
+  echo -e $'\e[1;33m\e[0m\e[1;33m    ██████████\e[0m'"\e[96m██████████"'\e[1;33m\e[0m\e[1;31m██████████\e[0m' '\e[1;32m\e[0m\e[1;32m by:SAD&TheAngel_XXNERO \e[0m''\e[1;37m\e[0m\e[1;37m [v1] \e[0m'                                       
+  echo ""
+   echo -e $'\e[1;33m\e[0m\e[1;33m  [ \e[0m\e[1;32m Follow on Github :- https://github.com/ \e[0m \e[1;32m\e[0m\e[1;33m] \e[0m'
+   echo ""
+      echo -e $'\e[1;37m\e[0m\e[1;37m    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ >>\e[0m'
+      echo -e "\e[93m    |B|Y|S|A|D| |N|E|R|O|-G|"      
+      echo -e $'\e[1;37m\e[0m\e[1;37m    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ >>\e[0m' 
+      echo ""                                                
+      }
+      banner
